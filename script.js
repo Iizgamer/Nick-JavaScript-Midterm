39956d7cd378ee6e9be67ec3dd0c79f498c79bf9
Nick <nickfoles09@icloud.com>
2026-03-10 14:07:58 -0400

    day 2: Lord please let me finish this

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let music = document.getElementById('music');
 const musicButton = document.getElementById('musicButton');
 const startButton = document.getElementById('startButton');
 const titleText = document.getElementById('title');
+const textBox = document.getElementById('textBox');
+const textBoxText = document.getElementById('textBoxText');
+const continueText = document.getElementById('continueText');
+const mcText = document.getElementById('mcText');
 
 musicButton.addEventListener('click', function() {
     music.play();
@@ -13,4 +17,10 @@ startButton.addEventListener('click', function() {
     startButton.style.display = 'none';
     titleText.innerHTML = '';
     music.src = './music/dieHard.mp3';
-})
+    textBox.style.backgroundColor = '#6a6a6a';
+    textBox.style.width = 'auto';
+    textBox.style.padding = '100px';
+    textBoxText.textContent = "Dang! This music is fiyah! Too bad it's unreleased...";
+    textBoxText.style.backgroundColor = '#6a6a6a';
+    continueText.style.display = 'block';
+});
