fe4e50fb1a5b90b737aea08c8dad7fc4f654efab
Nicholas Foles _ Student - WakeIBTechEC <npfoles@students.wcpss.net>
2026-03-09 22:23:39 -0400

    Day 1: Added music, might work on more

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+titleMusic = document.getElementById('titleMusic');
+musicButton = document.getElementById('musicButton');
 
+musicButton.addEventListener('click', function() {
+    titleMusic.play();
+})
