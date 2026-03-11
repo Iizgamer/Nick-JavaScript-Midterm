aeadece1656aca1204ba9a2432669ff13a753494
Nick <nickfoles09@icloud.com>
2026-03-11 14:08:37 -0400

    da helm

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const titleText = document.getElementById('title');
 const textBox = document.getElementById('textBox');
 const textBoxText = document.getElementById('textBoxText');
 const continueText = document.getElementById('continueText');
-const mcText = document.getElementById('mcText');
+let sceneCount = 0;
 
 musicButton.addEventListener('click', function() {
     music.play();
@@ -23,4 +23,23 @@ startButton.addEventListener('click', function() {
     textBoxText.textContent = "Dang! This music is fiyah! Too bad it's unreleased...";
     textBoxText.style.backgroundColor = '#6a6a6a';
     continueText.style.display = 'block';
+    document.body.background = './images/background-1.png';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+});
+
+continueText.addEventListener('click', function() {
+    sceneCount++;
+    if (sceneCount === 1) {
+        textBoxText.textContent = "Wait! I got it! I'll just steal the music and release it myself!";
+    } else if (sceneCount === 2) {
+        textBoxText.textContent = "I'll just have to get to the studio, take the records, and leave! Should be easy!";
+    } else if (sceneCount === 3) {
+        document.body.background = './images/studio.png';
+        document.body.style.backgroundRepeat = 'no-repeat';
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundPosition = 'center';
+        textBoxText.textContent = "Alright! I'm here! Now, let's see... where are those records..."
+    }
 });
